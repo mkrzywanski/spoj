0966e402cbdb8b79d65432083401148e00cfccba
michal <michak964@gmail.com>
2018-07-11 16:17:46 +0100

    NWD

---
+++ src/spoj/NWD.java
@@ -0,0 +1,29 @@
+package spoj;
+
+import java.util.Scanner;
+
+public class NWD {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int testsNumber = Integer.valueOf(scanner.nextLine());
+
+        for(int i = 0; i < testsNumber; i++) {
+            String[] numbers = scanner.nextLine().split(" ");
+
+            int a = Integer.valueOf(numbers[0]);
+            int b = Integer.valueOf(numbers[1]);
+
+            System.out.println(nwd(a, b));
+        }
+    }
+
+    private static int nwd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        else {
+            return nwd(b, a % b);
+        }
+    }
+}

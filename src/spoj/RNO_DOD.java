0d7ab8f543e95aa833ba133769244eb2940df2ef
michal <michak964@gmail.com>
2018-07-11 12:41:31 +0100

    RNO_DOD

--- src/spoj/RNO_DOD.java
+++ src/spoj/RNO_DOD.java
@@ -1,7 +1,25 @@
 package spoj;
 
+import java.util.Scanner;
+
 public class RNO_DOD {
     public static void main(String... args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int testsNumber = Integer.valueOf(scanner.nextLine());
+
+        int numbersAmount;
+        int sum;
+        for(int i = 0; i < testsNumber; i++) {
+            numbersAmount = Integer.parseInt(scanner.nextLine());
+
+            String[] numbers = scanner.nextLine().split(" ");
+            sum = 0;
+            for(int numberIndex = 0; numberIndex < numbersAmount; numberIndex++) {
+                sum += Integer.valueOf(numbers[numberIndex]);
+            }
+
+            System.out.println(sum);
+        }
     }
 }
